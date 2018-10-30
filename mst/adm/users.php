04b14582fbe76e6e6b79d4427af8355c93df8e2e
Milan Dian <dian.milan@seznam.cz>
2018-10-31 00:05:01 +0100

    v h 181030

--- mst/adm/users.php
+++ mst/adm/users.php
@@ -1,27 +1,34 @@
 <HTML>
 <HEAD>
-<TITLE>Výpis uživatelù</TITLE>
+<TITLE>VÃ½pis uÅ¾ivatelÅ¯</TITLE>
 </HEAD>
 <BODY>
-<H1>Výpis uživatelù</H1>
+<H1>VÃ½pis uÅ¾ivatelÅ¯</H1>
 <?php
-$mysqli = new mysqli('localhost', 'strj', 'str1', 'mst');
+$mysqli = new mysqli('127.0.0.1', 'root', '', 'mst');
 
 if ($mysqli->connect_error) {
-    die('Nepodaøilo se pøipojit k MySQL serveru (' . $mysqli->connect_errno . ') '
+    die('NepodaÅ™ilo se pÅ™ipojit k MySQL serveru (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
 
-$vysledek = $mysqli->query("SELECT * FROM `users`");
-echo 'Z databáze jsme získali ' . $vysledek->num_rows . ' uživatelù.';
+$result = $mysqli->query("SELECT * FROM `users`");
+echo 'Z databÃ¡ze jsme zÃ­skali ' . $result->num_rows . ' uÅ¾ivatelÅ¯.<br><br>';
 
-while ($uzivatel = $vysledek->fetch_assoc())
-{
-  printf("%s %s \n", $uzivatel['ID'], $uzivatel['UID'], $uzivatel['login_name'], $uzivatel['on_map']);
+if ($result->num_rows > 0) {
+    echo '<table><tr><td>ID</td><td>UID</td><td>login_name</td><td>on_map</td></tr>';
+
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["ID"]. "</td><td>" . $row["UID"]. "</td><td>" . $row["login_name"]. "</td><td>" . $row["on_map"]. "</td></tr>";
+    }
+    
+    echo '</table>';
+} else {
+    echo "0 results";
 }
-$vysledek->free_result();
 
-
+    
 $mysqli->close();
 ?>
 
