cecb4f11632b6c39a5df0b37f7990351e652c9b9
oremir <oremir@gmx.com>
2020-01-02 15:49:03 +0100

    v h 1.202 200102

--- mst/js/main.js
+++ mst/js/main.js
@@ -3,10 +3,6 @@ var Mst = Mst || {};
 
 var game = new Phaser.Game(550, 400, Phaser.CANVAS);
 
-//Phaser.Device.whenReady(function () {
-//    game.plugins.add(PhaserInput.plugin);
-//});
-
 game.state.add("BootState", new Mst.BootState());
 game.state.add("LoadingState", new Mst.LoadingState());
 game.state.add("GameState", new Mst.TiledState());
